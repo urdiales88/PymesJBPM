be6d141892abea0aec6a00ba9f0d26e73fb1f4fd
bpmsAdmin <>
2017-11-07 02:47:59 +0100

    {/MFAD_PYMES/src/main/java/com/bbva/mfad/pymes/VentasAnuales.java}

--- MFAD_PYMES/src/main/java/com/bbva/mfad/pymes/VentasAnuales.java
+++ MFAD_PYMES/src/main/java/com/bbva/mfad/pymes/VentasAnuales.java
@@ -6,12 +6,45 @@ package com.bbva.mfad.pymes;
 
 public class VentasAnuales implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public VentasAnuales() {
-    }
+	private java.lang.String ventasAnuales;
+	private java.lang.String segmentacionCliente;
+	private com.bbva.mfad.pymes.Depositos depositos;
 
+	public VentasAnuales() {
+	}
 
+	public java.lang.String getVentasAnuales() {
+		return this.ventasAnuales;
+	}
 
+	public void setVentasAnuales(java.lang.String ventasAnuales) {
+		this.ventasAnuales = ventasAnuales;
+	}
+
+	public java.lang.String getSegmentacionCliente() {
+		return this.segmentacionCliente;
+	}
+
+	public void setSegmentacionCliente(java.lang.String segmentacionCliente) {
+		this.segmentacionCliente = segmentacionCliente;
+	}
+
+	public com.bbva.mfad.pymes.Depositos getDepositos() {
+		return this.depositos;
+	}
+
+	public void setDepositos(com.bbva.mfad.pymes.Depositos depositos) {
+		this.depositos = depositos;
+	}
+
+	public VentasAnuales(java.lang.String ventasAnuales,
+			java.lang.String segmentacionCliente,
+			com.bbva.mfad.pymes.Depositos depositos) {
+		this.ventasAnuales = ventasAnuales;
+		this.segmentacionCliente = segmentacionCliente;
+		this.depositos = depositos;
+	}
 
 }
